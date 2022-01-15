5db231edcbf784b87571c8f4b12106de5d16810c
BeyazGolge <husamcahit@gmail.com>
2022-01-15 04:43:16 +0300

    corrected bugs

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -30,38 +30,36 @@ exports.registerUser = async (req, res) => {
 
 exports.loginUser = async (req, res) => {
   let token = req.cookies.auth;
-  token
-    ? User.findByToken(token, (err, user) => {
-        if (err) return res(err);
-        if (user)
-          return res.status(400).json({
-            error: true,
-            message: "You are already logged in",
+  const user = User.findByToken(token, (err, user) => {
+    if (err) return res(err);
+    if (user)
+      return res.status(400).json({
+        error: true,
+        message: "You are already logged in",
+      });
+    else {
+      User.findOne({ email: req.body.email }, function (err, user) {
+        if (!user)
+          return res.json({
+            isAuth: false,
+            message: " Auth failed ,email not found",
           });
-        else {
-          User.findOne({ email: req.body.email }, function (err, user) {
-            if (!user)
-              return res.json({
-                isAuth: false,
-                message: " Auth failed ,email not found",
-              });
 
-            user.comparepassword(req.body.password, (err, isMatch) => {
-              if (!isMatch)
-                return res.json({
-                  isAuth: false,
-                  message: "password doesn't match",
-                });
-
-              user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("auth", user.token).redirect("/api/dashboard");
-              });
+        user.comparepassword(req.body.password, (err, isMatch) => {
+          if (!isMatch)
+            return res.json({
+              isAuth: false,
+              message: "password doesn't match",
             });
+
+          user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("auth", user.token).redirect("/api/dashboard");
           });
-        }
-      })
-    : req.send("No token found you must log in first");
+        });
+      });
+    }
+  });
 };
 
 exports.getDashboardPage = async (req, res) => {
