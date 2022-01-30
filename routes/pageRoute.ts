d1e193f6881dc34bef9af9dfe582ad88a9d7bc15
BeyazGolge <husamcahit@gmail.com>
2022-01-30 13:24:08 +0300

    Cleared

--- routes/pageRoute.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import * as pageController from '../controllers/pageController';
-
-router.route('/').get(pageController.getIndexPage);
-export default router;
